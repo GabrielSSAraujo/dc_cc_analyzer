7f45d89516ca7afdddabf37c2bd4fe87c3d61754
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2024-11-18 19:43:45 -0300

    bugfix: coupling identification

--- models/coupling_list.py
+++ models/coupling_list.py
@@ -7,9 +7,12 @@ from .parameter import Parameter
 class Coupling:
     function_a: Optional[str] = None
     function_b: Optional[str] = None
-    coord_a: List[int] = field(default_factory=list)
-    coord_b: List[int] = field(default_factory=list)
     parameters: List[Parameter] = field(default_factory=list)
 
+    def __eq__(self, coupling):
+        return (coupling.function_a == self.function_a) and (
+            coupling.function_b == self.function_b
+        )
+
     def __repr__(self):
-        return f"{self.function_a} -> {self.function_b} : {self.coord_a}, {self.coord_b} -> {self.parameters}"
+        return f"{self.function_a} -> {self.function_b} -> {self.parameters}"
