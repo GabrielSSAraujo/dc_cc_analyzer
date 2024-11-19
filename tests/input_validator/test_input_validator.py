e1c78eb201915665231500c3695f8a612276d898
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2024-11-19 12:26:28 -0300

    add a script to discover and run all tests

--- tests/input_validator/test_input_validator.py
+++ tests/input_validator/test_input_validator.py
@@ -13,6 +13,7 @@ from modules.input_validator.input_validator import InputValidator
 # Get absolute path of data for the tests
 data_path = os.path.join(os.path.dirname(__file__), "..", "data")
 
+
 class TestInputValidator(unittest.TestCase):
 
     def test_req3_valid_argv(self):
@@ -24,7 +25,9 @@ class TestInputValidator(unittest.TestCase):
     def test_req3_less_than_3_args(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertFalse(iv.validate_argv())
-        self.assertEqual(mock_stdout.getvalue(), "[ERROR] Missing one or more tool inputs\n")
+        self.assertEqual(
+            mock_stdout.getvalue(), "[ERROR] Missing one or more tool inputs\n"
+        )
         del iv
 
     def test_req4_files_exist(self):
@@ -36,19 +39,22 @@ class TestInputValidator(unittest.TestCase):
     def test_req4_file_doesnot_exist(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/main2.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertFalse(iv.validate_files())
-        self.assertEqual(mock_stdout.getvalue(), f"[ERROR] Could not find SUT input file at path {data_path}/main2.c\n")
+        self.assertEqual(
+            mock_stdout.getvalue(),
+            f"[ERROR] Could not find SUT input file at path {data_path}/main2.c\n",
+        )
         del iv
     
     def test_req5_SUT_in_C(self):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertTrue(iv.validate_sut())
         del iv
-    
+
     def test_req8_TestVec_CSV(self):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
         self.assertTrue(iv.validate_test_vectors_file())
         del iv
-    
+
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_req8_TestVec_TXT(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.txt"])
@@ -60,7 +66,7 @@ class TestInputValidator(unittest.TestCase):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
         self.assertTrue(iv.validate_test_vectors_format())
         del iv
-    
+
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_req7_TestVec_without_tolerances(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_tolerances.csv"])
@@ -89,5 +95,6 @@ class TestInputValidator(unittest.TestCase):
         self.assertEqual(mock_stdout.getvalue(), "[ERROR] Bad Test Vector format\n")
         del iv
 
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+
+# if __name__ == "__main__":
+#     unittest.main(verbosity=2)
