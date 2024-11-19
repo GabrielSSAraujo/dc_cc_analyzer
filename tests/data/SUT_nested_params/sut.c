5a78f3e5a7965fb4d9e7ace7dc360d57d852aa9e
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2024-11-19 20:11:12 -0300

    create test to verify the mapping of which coupling influences each output

--- tests/data/SUT_nested_params/sut.c
+++ tests/data/SUT_nested_params/sut.c
@@ -6,33 +6,39 @@ void compA(float suti0, double suti1, int suti2, int *AO1, double *AO2)
     *AO2 = suti0 + suti2;
 }
 
-void compB(char suit3, int suti4, float suti5, double suti6, float *BO1)
+void compB(char suti3, int suti4, float suti5, double suti6, float *BO1)
 {
-    *BO1 = suit3 + suti4 + suti5 + (float)suti6;
+    *BO1 = suti3 + suti4 + suti5 + (float)suti6;
 }
 
-void compC(int AO1, float BO1, double *CO1, double *suto2)
+void compC(int *AO1, float BO1, char *CO1, double *suto2)
 {
-    *CO1 = AO1 + BO1;
-    *suto2 = *CO1;
+    *CO1 = (char)(*AO1 + BO1);
+    *suto2 = *CO1 + BO1;
 }
 
-void compD(double CO1, double AO2, int *suto1)
+void compD(char CO1, double AO2, int *suto1)
 {
     *suto1 = (int)(CO1 + AO2);
 }
 
-void compE(double *eo, int ei)
+int compE(double *eo, int ei)
 {
     *eo += ei;
+    return (int)*eo;
 }
 
-void sut(float suti0, double suti1, int suti2, char suti3, int suti4, float suti5, double suti6, int *suto1, double *suto2, float *suto3, char *suto4, int *suto5, double *suto6)
+void sut(float suti0, double suti1, int suti2, char suti3, int suti4, float suti5, double suti6, int *suto1, double *suto2)
 {
     int ei = 10;
-    compA(suti0, suti1, suti2, suto1, suto2);
-    compB(suti3, suti4, suti5, suti6, suto3);
-    compE(suto2, ei);
-    compC(*suto1, *suto3, suto6, suto2);
-    compD(*suto6, *suto2, suto1);
+    int a1;        
+    double a2;      
+    float a3;      
+    char a4;       
+
+    compA(suti0, suti1, suti2, &a1, &a2);   
+    compB(suti3, suti4, suti5, suti6, &a3); 
+    *suto1 = compE(&a2, ei);                
+    compC(&a1, a3, &a4, suto2);             
+    compD(a4, a2, suto1);                   
 }
---
+++ tests/data_couplings_flow/test_data_couplings_flow.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import sys
+
+
+# Encontra o diretório raiz do projeto (dois níveis acima do arquivo atual)
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.insert(0, project_root)  # Insere o diretório raiz no início do sys.path
+# Get full absolute path of data for the tests
+data_path = os.path.join(os.path.dirname(__file__), "..", "data")
+
+from modules.analyzer.static_analyzer import StaticAnalyzer
+from modules.data_couplings_flow.data_couplings_flow import DataCouplingFlow
+from models.parameter import Parameter
+
+
+class TestDataCouplingsFlow(unittest.TestCase):
+    def setUp(self):
+        static_analyzer = StaticAnalyzer()
+        ast = static_analyzer.get_ast(data_path + "/SUT_nested_params/sut.c")
+        self.coupled_data = static_analyzer.get_coupled_data(ast)
+        self.functions = static_analyzer.get_func_metadata()
+
+    def test_req16_output_to_couplings_map(self):
+
+        reference_mapping = {
+            "suto1": ["a1", "a2", "a2_aux", "a3", "a4"],
+            "suto2": ["a1", "a3"],
+        }
+
+        df = DataCouplingFlow(self.coupled_data, self.functions)
+        df.analyze_data_flow()
+        output_mapping = df.get_coupling_to_output_mapping()
+
+        for ref_key, _ in reference_mapping.items():
+            self.assertIn(ref_key, output_mapping)
