146593abbfdde89f0e89f993240be880bf1aeea2
BrunoColturato <brcolturato@gmail.com>
2024-11-20 08:59:09 -0300

    Tests for REQ-3 to REQ-7

--- tests/input_validator/test_input_validator.py
+++ tests/input_validator/test_input_validator.py
@@ -1,83 +1,90 @@
+# How to run: python3 test_input_validator.py
+# Run it on Linux
+
 import io
 import unittest
 from unittest.mock import patch
-
-# Add InputValidator module to the path and import it
 import sys
 import os
-module_path = os.path.join(os.path.dirname(__file__), "..", "..", "modules", "input_validator")
-sys.path.insert(0, module_path)
+sys.path.insert(0, "../..") # Include "modules" package
 
-from input_validator import InputValidator
+from modules.input_validator.input_validator import InputValidator
 
-# Get full absolute path of data for the tests
+# Get absolute path of data for the tests
 data_path = os.path.join(os.path.dirname(__file__), "..", "data")
 
 class TestInputValidator(unittest.TestCase):
 
-    def test_req5_valid_argv(self):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
+    def test_req3_valid_argv(self):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertTrue(iv.validate_argv())
         del iv
 
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_req5_less_than_3_args(self, mock_stdout):
+    def test_req3_less_than_3_args(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertFalse(iv.validate_argv())
         self.assertEqual(mock_stdout.getvalue(), "[ERROR] Missing one or more tool inputs\n")
         del iv
 
-    def test_req6_files_exist(self):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
+    def test_req4_files_exist(self):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertTrue(iv.validate_files())
         del iv
 
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_req6_file_doesnot_exist(self, mock_stdout):
+    def test_req4_file_doesnot_exist(self, mock_stdout):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/main2.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertFalse(iv.validate_files())
         self.assertEqual(mock_stdout.getvalue(), f"[ERROR] Could not find SUT input file at path {data_path}/main2.c\n")
         del iv
     
-    def test_req7_SUT_in_C(self):
+    def test_req5_SUT_in_C(self):
         iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.xlsx"])
         self.assertTrue(iv.validate_sut())
         del iv
     
     def test_req8_TestVec_CSV(self):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
         self.assertTrue(iv.validate_test_vectors_file())
         del iv
     
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_req8_TestVec_TXT(self, mock_stdout):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.txt"])
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.txt"])
         self.assertFalse(iv.validate_test_vectors_file())
-        self.assertEqual(mock_stdout.getvalue(), "[ERROR] Only Test Vectors in .xlsx or .csv are accepted\n")
+        self.assertEqual(mock_stdout.getvalue(), "[ERROR] Only Test Vectors in .xlsx, .xls or .csv are accepted\n")
         del iv
 
-    def test_req9_good_TestVec(self):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
+    def test_req7_good_TestVec(self):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01.csv"])
         self.assertTrue(iv.validate_test_vectors_format())
         del iv
     
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_req9_TestVec_without_tolerances(self, mock_stdout):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_tolerances.csv"])
+    def test_req7_TestVec_without_tolerances(self, mock_stdout):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_tolerances.csv"])
         self.assertFalse(iv.validate_test_vectors_format())
         self.assertEqual(mock_stdout.getvalue(), "[ERROR] Bad Test Vector format\n")
         del iv
 
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_req9_TestVec_without_inputcomments(self, mock_stdout):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_inputcomments.csv"])
+    def test_req7_TestVec_without_inputcomments(self, mock_stdout):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_inputcomments.csv"])
         self.assertFalse(iv.validate_test_vectors_format())
         self.assertEqual(mock_stdout.getvalue(), "[ERROR] Bad Test Vector format\n")
         del iv
 
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_req9_TestVec_without_time(self, mock_stdout):
-        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/SUT.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_time.csv"])
+    def test_req7_TestVec_without_outputcomments(self, mock_stdout):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_ouputcomments.csv"])
+        self.assertFalse(iv.validate_test_vectors_format())
+        self.assertEqual(mock_stdout.getvalue(), "[ERROR] Bad Test Vector format\n")
+        del iv
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_req7_TestVec_without_time(self, mock_stdout):
+        iv = InputValidator(["dc_cc_analyzer", f"{data_path}/SUT/sut.c", f"{data_path}/test_vectors/TestVec_VCP-500-VC-01_without_time.csv"])
         self.assertFalse(iv.validate_test_vectors_format())
         self.assertEqual(mock_stdout.getvalue(), "[ERROR] Bad Test Vector format\n")
         del iv
